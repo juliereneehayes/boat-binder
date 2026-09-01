4f308852e89ebda4be1168f7ab17e2212e17610f
Renee Hayes <juliereneehayes@gmail.com>
2026-09-01 06:44:20 -0700

    Prevent authenticated session switching

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+  before_action :redirect_authenticated_user, only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
 
   def new
@@ -22,5 +23,11 @@ class SessionsController < ApplicationController
   def destroy
     terminate_session
     redirect_to new_session_path, status: :see_other
+  end
+
+  private
+
+  def redirect_authenticated_user
+    redirect_to root_path if authenticated?
   end
 end
